368d7ddc4b68a4ecc91612b736c8ffc87e4ffc74
Arthur Alves <arthur.alvesdeveloper@outlook.com>
2021-08-03 13:29:29 -0300

    alteração do pom porém ainda persiste o erro

--- simulacao/src/main/java/br/com/orquestrador/SimulacaoApplication.java
+++ simulacao/src/main/java/br/com/orquestrador/SimulacaoApplication.java
@@ -1,9 +1,12 @@
 package br.com.orquestrador;
 
+import io.grpc.netty.shaded.io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.TypeHint;
 
 @SpringBootApplication
+@TypeHint(types = NioServerSocketChannel.class, typeNames = "io.grpc.netty.shaded.io.netty.channel.socket.nio.NioServerSocketChannel$NioServerSocketChannelConfig")
 public class SimulacaoApplication {
 
 	public static void main(String[] args) {
